0cc2dfd4a80890ae1a4cc51e1a625287b76a6c9a
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-03-30 01:46:27 +0200

    add tentacles export and pack commands

--- octobot_tentacles_manager/cli.py
+++ octobot_tentacles_manager/cli.py
@@ -19,7 +19,7 @@ import sys
 import aiohttp
 
 from octobot_commons.logging.logging_util import get_logger
-from octobot_tentacles_manager.api.creator import start_tentacle_creator
+from octobot_tentacles_manager.api.creator import start_tentacle_creator, create_tentacles_package
 from octobot_tentacles_manager.api.installer import install_all_tentacles, install_tentacles, \
     USER_HELP as INSTALL_USER_HELP, install_single_tentacle, repair_installation
 from octobot_tentacles_manager.api.uninstaller import uninstall_all_tentacles, uninstall_tentacles, \
@@ -30,16 +30,25 @@ from octobot_tentacles_manager.constants import DEFAULT_TENTACLES_URL, DEFAULT_B
 from octobot_tentacles_manager import PROJECT_NAME
 
 
-async def _handle_package_manager_command(starting_args, tentacles_url, target_dir,
-                                          single_tentacle_path, single_tentacle_type) -> int:
+async def _handle_package_manager_command(starting_args,
+                                          tentacles_url,
+                                          target_dir,
+                                          single_tentacle_path,
+                                          single_tentacle_type,
+                                          export_tentacles_output,
+                                          packed_tentacles_output) -> int:
     error_count = 0
     LOGGER = get_logger(f"{PROJECT_NAME}-CLI")
     async with aiohttp.ClientSession() as aiohttp_session:
         if starting_args.creator:
             error_count = start_tentacle_creator({}, starting_args.creator)
-        if starting_args.repair:
+        elif starting_args.repair:
             error_count = await repair_installation(bot_path=target_dir)
-        if single_tentacle_path:
+        elif starting_args.export:
+            error_count = await create_tentacles_package(export_tentacles_output, target_dir, in_zip=False)
+        elif starting_args.pack:
+            error_count = await create_tentacles_package(packed_tentacles_output, target_dir, in_zip=True)
+        elif single_tentacle_path:
             error_count = await install_single_tentacle(single_tentacle_path,
                                                         single_tentacle_type,
                                                         bot_path=target_dir,
@@ -81,15 +90,26 @@ async def _handle_package_manager_command(starting_args, tentacles_url, target_d
     return 0
 
 
-def handle_tentacles_manager_command(starting_args, tentacles_url=DEFAULT_TENTACLES_URL,
-                                     single_tentacle_path=None, single_tentacle_type=None) -> int:
+def handle_tentacles_manager_command(starting_args,
+                                     tentacles_url=DEFAULT_TENTACLES_URL,
+                                     single_tentacle_path=None,
+                                     single_tentacle_type=None,
+                                     export_tentacles_output=None,
+                                     packed_tentacles_output=None) -> int:
     tentacles_url = starting_args.location[0] if starting_args.location else tentacles_url
     target_bot_dir = starting_args.directory[0] if starting_args.directory else DEFAULT_BOT_PATH
     if starting_args.single_tentacle_install:
         single_tentacle_path = starting_args.single_tentacle_install[0]
         single_tentacle_type = starting_args.single_tentacle_install[1]
-    return asyncio.run(_handle_package_manager_command(starting_args, tentacles_url, target_bot_dir,
-                                                       single_tentacle_path, single_tentacle_type))
+    export_tentacles_output = starting_args.export[0] if starting_args.export else export_tentacles_output
+    packed_tentacles_output = starting_args.pack[0] if starting_args.pack else packed_tentacles_output
+    return asyncio.run(_handle_package_manager_command(starting_args,
+                                                       tentacles_url,
+                                                       target_bot_dir,
+                                                       single_tentacle_path,
+                                                       single_tentacle_type,
+                                                       export_tentacles_output,
+                                                       packed_tentacles_output))
 
 
 def register_tentacles_manager_arguments(tentacles_parser) -> None:
@@ -99,18 +119,26 @@ def register_tentacles_manager_arguments(tentacles_parser) -> None:
     tentacles_parser.add_argument("-r", "--repair", help="Repair a tentacles installation. "
                                                          "Fixes __init__.py files, missing main folders "
                                                          "and configuration files.", action='store_true')
-    tentacles_parser.add_argument("-a", "--all", help="Apply command to all available Tentacles.", action='store_true')
-    tentacles_parser.add_argument("-f", "--force", help="Skip user confirmations.", action='store_true')
-    tentacles_parser.add_argument("-l", "--location", help="Tentacles package local path or url to find the "
-                                                           "tentacles package to process.", nargs=1)
     tentacles_parser.add_argument("-sti", "--single-tentacle-install", help='Single tentacle to install identified by '
                                                                             'a local path and its tentacle type.\n'
                                                                             'Example: -sti "/bot/macd_eval" '
                                                                             '"Evaluator/TA".',
                                   nargs=2)
+    tentacles_parser.add_argument("-p", "--pack", help="Create a tentacle package containing all tentacles "
+                                                       "in the given folder.\n"
+                                                       "Specify source folder using the --directory argument\n"
+                                                       "Example: -p myTentaclesPackage.zip -d ./tentacles", nargs=1)
+    tentacles_parser.add_argument("-e", "--export", help="Export tentacles into a folder containing all tentacles "
+                                                         "in the given folder. Removes installation generated files."
+                                                         "Specify source folder using the --directory argument\n"
+                                                         "Example: -e myTentaclesFolder -d ./tentacles", nargs=1)
+    tentacles_parser.add_argument("-a", "--all", help="Apply command to all available Tentacles.", action='store_true')
     tentacles_parser.add_argument("-d", "--directory", help=f"Path to the root of the OctoBot installation folder "
                                                             f"to operate on. Default is '{DEFAULT_BOT_PATH}'.",
                                   nargs=1)
+    tentacles_parser.add_argument("-l", "--location", help="Tentacles package local path or url to find the "
+                                                           "tentacles package to process.", nargs=1)
+    tentacles_parser.add_argument("-f", "--force", help="Skip user confirmations.", action='store_true')
     tentacles_parser.add_argument("-c", "--creator", help="Start OctoBot Tentacles Creator.\n Examples: -c Evaluator "
                                                           "to create a new evaluator tentacles. Use: -c help to get the"
                                                           " Tentacle Creator help.", nargs='+')
