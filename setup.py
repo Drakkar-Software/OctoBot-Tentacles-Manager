d0f9f3036468b92f93a9882c656c372ac5676d6f
Herklos <herklos.dev@protonmail.com>
2020-10-23 19:45:06 +0200

    [Compatibility] Add python 3.8 support

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('README.md', encoding='utf-8') as f:
     DESCRIPTION = f.read()
 
 REQUIRED = open('requirements.txt').readlines()
-REQUIRES_PYTHON = '>=3.7'
+REQUIRES_PYTHON = '>=3.8'
 
 setup(
     name=PROJECT_NAME,
@@ -54,7 +54,11 @@ setup(
         ]
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3.7',
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: OS Independent',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.8',
     ],
 )
