39add29b52611fc2c61cf20fcad122fbb801b5eb
Herklos <herklos.dev@protonmail.com>
2020-10-25 10:23:33 +0100

    [Version] v2.3.3

--- octobot_tentacles_manager/__init__.py
+++ octobot_tentacles_manager/__init__.py
@@ -14,5 +14,5 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 
-VERSION = "2.3.2"
+VERSION = "2.3.3"
 PROJECT_NAME = "OctoBot-Tentacles-Manager"
