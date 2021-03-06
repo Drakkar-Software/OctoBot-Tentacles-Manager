f42ea43c052a98a7242823218864ecb90bdc0942
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-03-06 14:53:46 +0100

    [Version] v2.4.8

--- octobot_tentacles_manager/__init__.py
+++ octobot_tentacles_manager/__init__.py
@@ -14,5 +14,5 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 
-VERSION = "2.4.7"
+VERSION = "2.4.8"
 PROJECT_NAME = "OctoBot-Tentacles-Manager"
